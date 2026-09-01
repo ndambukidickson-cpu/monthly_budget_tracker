4a3ba951cd8e76d18c0227aa291f8aee8a6408ed
ndambukidickson-cpu <ndambukidickson@gmail.com>
2026-09-01 14:00:02 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'budget-pwa-v1';
+const ASSETS = [
+  './budget.html',
+  './manifest.json',
+  'https://unpkg.com/dexie@latest/dist/dexie.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
